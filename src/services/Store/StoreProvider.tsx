e93dd44b521cbc7e17d2b419e6ee5ce1ca28ab98
Guillaume Roux <guillaumeroux123@gmail.com>
2020-08-03 16:37:59 +0200

    enhance and fix account labelisation on add

--- src/services/Store/StoreProvider.tsx
+++ src/services/Store/StoreProvider.tsx
@@ -68,6 +68,7 @@ import { NetworkContext, getNetworkById } from './Network';
 import { findNextUnusedDefaultLabel, AddressBookContext } from './AddressBook';
 import { MyCryptoApiService, ANALYTICS_CATEGORIES } from '../ApiService';
 import { findMultipleNextUnusedDefaultLabels } from './AddressBook/helpers';
+import { translateRaw } from '@translations';
 
 export interface CoinGeckoManifest {
   [uuid: string]: string;
@@ -429,10 +430,12 @@ export const StoreProvider: React.FC = ({ children }) => {
       newRawAccounts.forEach((rawAccount, idx) => {
         const existingContact = getContactByAddressAndNetworkId(rawAccount.address, networkId);
         if (existingContact) {
-          updateContact(existingContact.uuid, {
-            ...existingContact,
-            label: newLabels[idx]
-          });
+          if (existingContact.label === translateRaw('NO_LABEL')) {
+            updateContact(existingContact.uuid, {
+              ...existingContact,
+              label: newLabels[idx]
+            });
+          }
         } else {
           const newLabel: AddressBook = {
             label: newLabels[idx],
